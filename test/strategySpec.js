7411db4d949d620f912017ae5ca5d95e18c57f6d
Andrew Kimpton <awk@onshape.com>
2015-06-18 11:42:35 -0400

    Initial commit.

---
+++ test/strategySpec.js
@@ -0,0 +1,60 @@
+var Strategy=require('../lib/index.js');
+
+describe('Strategy Tests', function () {
+  var onshapeStrategy;
+  beforeEach(function() {
+    onshapeStrategy = new Strategy({
+      clientID: 'AClientID',
+      clientSecret: 'TheClientSecret',
+      callbackURL: 'http://example.com/oauthCallback',
+      userAgent: 'TheUserAgent'
+      }, function() {});
+  });
+
+  it('should be able to construct the Onshape strategy', function() {
+
+    expect(onshapeStrategy.name).toEqual('onshape');
+  });
+
+  it('should fetch the user profile', function() {
+    var done = jasmine.createSpy('done');
+    spyOn(onshapeStrategy._oauth2, 'get').andCallFake(function(url, token, cb) {
+      var response = {id: 'AUserId', name: 'User Name',html_url: 'http://example.com', email: 'user@example.com'};
+      cb(null, JSON.stringify(response));
+    });
+    onshapeStrategy.userProfile('AnAccessToken', done);
+    expect(onshapeStrategy._oauth2.get).toHaveBeenCalledWith(jasmine.any(String), 'AnAccessToken', jasmine.any(Function));
+    expect(done).toHaveBeenCalledWith(null, {
+      provider: 'onshape',
+      id: 'AUserId',
+      displayName: 'User Name',
+      profileUrl: 'http://example.com',
+      emails: [{value: 'user@example.com'}],
+      _raw: jasmine.any(String),
+      _json: jasmine.any(Object)
+    });
+  });
+
+  it('should handle an error when fetching the user profile', function() {
+    var done = jasmine.createSpy('done');
+    spyOn(onshapeStrategy._oauth2, 'get').andCallFake(function(url, token, cb) {
+      cb('An Error occured', null);
+    });
+    onshapeStrategy.userProfile('AnAccessToken', done);
+    expect(onshapeStrategy._oauth2.get).toHaveBeenCalledWith(jasmine.any(String), 'AnAccessToken', jasmine.any(Function));
+    expect(done).toHaveBeenCalledWith({
+      name : 'InternalOAuthError',
+      message : 'Failed to fetch user profile',
+      oauthError : 'An Error occured' });
+  });
+
+  it('should handle an error when deserializing the user profile', function() {
+    var done = jasmine.createSpy('done');
+    spyOn(onshapeStrategy._oauth2, 'get').andCallFake(function(url, token, cb) {
+      cb(null, 'A Text String');
+    });
+    onshapeStrategy.userProfile('AnAccessToken', done);
+    expect(onshapeStrategy._oauth2.get).toHaveBeenCalledWith(jasmine.any(String), 'AnAccessToken', jasmine.any(Function));
+    expect(done).toHaveBeenCalledWith({});
+  });
+});
